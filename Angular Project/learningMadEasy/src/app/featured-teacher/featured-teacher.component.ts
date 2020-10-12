3b475d204925321043242e74d2fdf521af26ed18
ravii96 <raviprakash392@gmail.com>
2020-10-13 02:22:41 +0530

    Angular setup

---
+++ Angular Project/learningMadEasy/src/app/featured-teacher/featured-teacher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-teacher',
+  templateUrl: './featured-teacher.component.html',
+  styleUrls: ['./featured-teacher.component.css']
+})
+export class FeaturedTeacherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
